312e80d0bda566e01e03c3f05053ac9d433d9ede
Milen Radkov <milenradkov@me.com>
2018-11-24 17:57:26 +0200

    rename function referralJoin -> referralRegister

--- contracts/ReferralInterface.sol
+++ contracts/ReferralInterface.sol
@@ -6,6 +6,6 @@ pragma solidity >=0.4.22 <0.6.0;
  */
 interface ReferralInterface {
 
-    function referralJoin(address account) external returns (bool);
+    function referralRegister(address account) external returns (bool);
 
 }
